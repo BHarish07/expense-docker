1072b2b3a09376aa5ad0136423e6d4868246b583
BHarish07 <harishbalike1995@gmail.com>
2024-06-21 19:19:07 +0530

    docker-expense

--- install-docker.sh
+++ install-docker.sh
@@ -1,8 +1,7 @@
 #Installation of docker for RHEL9
-#run this using root user
 #we can run this in the instance by following cmd
 #curl https://raw.githubusercontent.com/BHarish07/expense-docker/main/install-docker.sh | sudo bash
-#sudo su -
+#the above commnd will directly run as root user
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
